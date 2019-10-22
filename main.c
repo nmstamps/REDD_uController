275fb23f5414e8d2ff09179a794c08aef0e31532
nmstamps <35670640+nmstamps@users.noreply.github.com>
2019-10-22 08:58:48 -0400

    added very basic main

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <ti/devices/msp432p4xx/driverlib/driverlib.h>
+
+setup()
+{
+}
+
+loop()
+{
+}
+
+fsm()
+{
+}
+
+
+
+void main(void) {
+
+  setup()
+  
+  while(1) {
+    
+	//loop
+	loop()
+	fsm()
+  }//end of while loop
+}//end of main
