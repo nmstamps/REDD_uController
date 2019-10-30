f3f60b2491ca8cddec88f8a24e5fc20761be379b
nmstamps <35670640+nmstamps@users.noreply.github.com>
2019-10-30 03:04:05 -0400

    Overhaul of all files. Now should work for MSP430 with a blinking LED

---
+++ Debug/blink.d
@@ -0,0 +1,15 @@
+# FIXED
+
+blink.obj: ../blink.c
+blink.obj: C:/ti/ccs920/ccs/ccs_base/msp430/include/msp430.h
+blink.obj: C:/ti/ccs920/ccs/ccs_base/msp430/include/msp430fr5969.h
+blink.obj: C:/ti/ccs920/ccs/ccs_base/msp430/include/in430.h
+blink.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics.h
+blink.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics_legacy_undefs.h
+
+../blink.c: 
+C:/ti/ccs920/ccs/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccs920/ccs/ccs_base/msp430/include/msp430fr5969.h: 
+C:/ti/ccs920/ccs/ccs_base/msp430/include/in430.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-msp430_18.12.3.LTS/include/intrinsics_legacy_undefs.h: 
